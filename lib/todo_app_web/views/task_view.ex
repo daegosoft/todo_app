74795ccaec91b99172a8a58571dd4e2b09263280
daegosoft <daegosoft@gmail.com>
2022-09-22 23:11:03 -0500

    use functions in html

--- lib/todo_app_web/views/task_view.ex
+++ lib/todo_app_web/views/task_view.ex
@@ -1,3 +1,11 @@
 defmodule TodoAppWeb.TaskView do
   use TodoAppWeb, :view
+
+  defp now() do
+    NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+  end
+
+  defp is_done(true), do: "It's done"
+  defp is_done(true), do: "Missin task"
+
 end
